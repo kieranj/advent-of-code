45bc9f65da00ee0bdbeb16d54512ab4a42e3b95f
Kristis Teniauskas <teniauskas.kristis@gmail.com>
2021-12-03 11:11:41 +0000

    First task completed

--- day1/main.rb
+++ day1/main.rb
@@ -1,9 +1,15 @@
 require 'enumerator'
 
+count = 0
 file = File.open('input.txt')
 
 lines = file.readlines.map(&:chomp).map(&:to_i)
 
-lines.each_slice(2) do |x|
-  
+lines.each_with_index do |x, index|
+  next if index == 0
+  if x > lines[index - 1]
+    count += 1
+  end
 end
+
+p count
