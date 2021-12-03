969db18b494f9ad435a363cb2c33f7ad0cced716
Kieran Johnson <kieran@invisiblelines.com>
2021-12-03 11:56:01 +0000

    Solution for day 2

--- day2/main.rb
+++ day2/main.rb
@@ -9,21 +9,28 @@ file.each_line do |line|
 
   value = value.to_i
 
+# down X increases your aim by X units.
+# up X decreases your aim by X units.
+# forward X does two things:
+# It increases your horizontal position by X units.
+# It increases your depth by your aim multiplied by X.
+
+# forward 5 adds 5 to your horizontal position, a total of 5. Because your aim is 0, your depth does not change.
+# down 5 adds 5 to your aim, resulting in a value of 5.
+# forward 8 adds 8 to your horizontal position, a total of 13. Because your aim is 5, your depth increases by 8*5=40.
+# up 3 decreases your aim by 3, resulting in a value of 2.
+# down 8 adds 8 to your aim, resulting in a value of 10.
+# forward 2 adds 2 to your horizontal position, a total of 15. Because your aim is 10, your depth increases by 2*10=20 to a total of 60.
+
   case command
-  when 'up'
-    depth -= value
-    aim -= value
   when 'down'
-    depth += value
     aim += value
+  when 'up'
+    aim -= value
   when 'forward'
     horizontal += value
-    depth += aim * value
+    depth = depth + (value * aim)
   end
-
-  p depth
-  p aim
-  p horizontal
 end
 
 puts horizontal * depth
